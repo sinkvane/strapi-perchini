8fdb4d37d6f835a1b64dac9313083b6b8aeaa57d
47work <sinkvadev@gmail.com.>
2025-07-24 18:56:02 +0700

    fixes

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,7 @@ module.exports = [
     config: {
       origin: [
         'http://localhost:5173',
-        'https://perchini-taplink-28i6dbqv3-sinkvanes-projects.vercel.app'
+        'https://perchini-taplink.vercel.app/'
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     },
