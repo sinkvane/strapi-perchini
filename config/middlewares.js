a30f197747bc67518d7e8d45654c5dac61bf4d8e
47work <sinkvadev@gmail.com.>
2025-07-24 17:59:50 +0700

    changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'https://perchini-taplink-28i6dbqv3-sinkvanes-projects.vercel.app'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
