fd5355802dfcf0f3e86c488ff19d73384df482a3
47work <sinkvadev@gmail.com.>
2025-08-13 14:30:59 +0700

    changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,10 +6,10 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       origin: [
-        // 'http://localhost:5173',
-        // 'https://perchini-taplink.vercel.app',
-        // 'https://co26422.tw1.ru'
-        '*'
+        'http://localhost:5173',
+        'http://localhost:45678',
+        'https://perchini-taplink.vercel.app',
+        'https://co26422.tw1.ru'
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     },
